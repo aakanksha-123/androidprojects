40fb738f0065cdffe3c5f7ae6a967945cc810115
aakanksha-123 <94225817+aakanksha-123@users.noreply.github.com>
2021-11-17 14:24:31 +0530

    Explicit Intent

---
+++ ExplicitIntent1/app/src/main/java/com/example/explicitintent1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.explicitintent1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent it =new Intent(this,MainActivity2.class);
+        startActivity(it);
+    }
+}
